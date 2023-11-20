7c95288e16e79053a964fb8ab004329f44057eb9
harshaveenar <113198739+harshaveenar@users.noreply.github.com>
2023-11-20 08:31:57 +0530

    Waiting Process with Smallest Execution Time.c

--- Waiting Process with Smallest Execution Time.c
+++ Waiting Process with Smallest Execution Time.c
@@ -1,51 +1,47 @@
-#include<stdio.h>
-void findWaitingTime(int processes[], int n, int bt[], int wt[], int tat[]) 
+#include <stdio.h>
+int main()
 {
-    wt[0] = 0;
-    
-    int i;
-    for ( i = 1; i < n; i++)
-        wt[i] = bt[i - 1] + wt[i - 1];
-    for (i = 0; i < n; i++)
-        tat[i] = bt[i] + wt[i];
-}
-void findAverageTime(int processes[], int n, int bt[]) {
-    int wt[n], tat[n];
+	int A[100][4];
+	int i, j, n, total = 0, index, temp;
+	float avg_wt, avg_tat;
+	printf("Enter number of process: ");
+	scanf("%d", &n);
+	printf("Enter Burst Time:\n");
+	for (i = 0; i < n; i++) {
+		printf("P%d: ", i + 1);
+		scanf("%d", &A[i][1]);
+		A[i][0] = i + 1;
+	}
+	for (i = 0; i < n; i++) {
+		index = i;
+		for (j = i + 1; j < n; j++)
+			if (A[j][1] < A[index][1])
+				index = j;
+		temp = A[i][1];
+		A[i][1] = A[index][1];
+		A[index][1] = temp;
 
-    findWaitingTime(processes, n, bt, wt, tat);
-
-    int total_wt = 0, total_tat = 0;
-    int i;
-    for ( i = 0; i < n; i++) {
-        total_wt += wt[i];
-        total_tat += tat[i];
-    }
-
-    float avg_wt = (float) total_wt / (float) n;
-    float avg_tat = (float) total_tat / (float) n;
-
-    printf("Average Waiting Time: %.2f\n", avg_wt);
-    printf("Average Turnaround Time: %.2f\n", avg_tat);
-}
-
-int main() {
-
-    int n;
-     int i;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-
-    int processes[n];
-    int burst_time[n];
-   
-    printf("Enter burst times for each process:\n");
-    for (i = 0; i < n; i++) {
-        printf("Burst time for process %d: ", i + 1);
-        scanf("%d", &burst_time[i]);
-        processes[i] = i + 1; // Process IDs start from 1
-    }
-
-    findAverageTime(processes, n, burst_time);
-
-    return 0;
+		temp = A[i][0];
+		A[i][0] = A[index][0];
+		A[index][0] = temp;
+	}
+	A[0][2] = 0;
+	for (i = 1; i < n; i++) {
+		A[i][2] = 0;
+		for (j = 0; j < i; j++)
+			A[i][2] += A[j][1];
+		total += A[i][2];
+	}
+	avg_wt = (float)total / n;
+	total = 0;
+	printf("P	 BT	 WT	 TAT\n");
+	for (i = 0; i < n; i++) {
+		A[i][3] = A[i][1] + A[i][2];
+		total += A[i][3];
+		printf("P%d	 %d	 %d	 %d\n", A[i][0],
+			A[i][1], A[i][2], A[i][3]);
+	}
+	avg_tat = (float)total / n;
+	printf("Average Waiting Time= %f", avg_wt);
+	printf("\nAverage Turnaround Time= %f", avg_tat);
 }
